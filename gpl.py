66a8cca5b643e7eb53dac2f33f2c18d7c88a962a
Steve <steve@wifijanitor.com>
2016-03-05 17:20:53 -0600

    Added files via upload

---
+++ gpl.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+#Important Imported Imports
+import credentials
+import requests
+import csv
+
+# Defining our variables. 
+#The credentials stored in credentials.py in the same directory as where this file is located
+# the files we are going to read and write too
+
+userid=credentials.username
+passwd=credentials.password
+
+in_file="/Cisco/glus.txt"
+out_file="/Cisco/price.txt"
+dest=open(out_file, "wb")
+
+url='http://www.cisco.com/web/lpc/ascii/glus.web'
+
+#-----------------------------------------------------------------------------------------
+
+# Request for the file, and authentication to the page
+
+s = requests.Session()
+s.auth = (userid, passwd)
+s.verify = False
+s.headers.update({'x-test': 'true'})
+
+print "Downloading the file"
+thatfile = s.get(url, headers={'x-test': 'true'})
+
+file = open(in_file, 'w')
+file.write(thatfile.content)
+file.close()
+
+#-----------------------------------------------------------------------------------------
+
+print "Grooming the File"
+with open(in_file, 'rb')as groom:
+	reader=csv.reader(groom, delimiter="|")
+	writer=csv.writer(dest, delimiter="|")
+
+	for row in reader:
+		if "Product" in row:
+			writer.writerow((row[3], row[4], row[6]))
+
+dest.close()
+groom.close()
