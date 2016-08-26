37bae0fbd21188f1c940461953a32ea581b20b70
wifijanitor <steve@wifijanitor.com>
2016-08-26 15:08:28 -0500

    minor update

--- gpl.py
+++ gpl.py
@@ -1,20 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-#Important Imported Imports
-import credentials
-import requests
-import csv
+import requests, csv, credentials
 
-# Defining our variables. 
-#The credentials stored in credentials.py in the same directory as where this file is located
-# the files we are going to read and write too
+#The credentials stored in credentials.py in the same directory as where this
+#file is located the files we are going to read and write too
 
 userid=credentials.username
 passwd=credentials.password
 
 in_file="/Cisco/glus.txt"
 out_file="/Cisco/price.txt"
-dest=open(out_file, "wb")
+dest=open(out_file, "wt")
 
 url='http://www.cisco.com/web/lpc/ascii/glus.web'
 
@@ -27,17 +23,17 @@ s.auth = (userid, passwd)
 s.verify = False
 s.headers.update({'x-test': 'true'})
 
-print "Downloading the file"
+print("Downloading the file")
 thatfile = s.get(url, headers={'x-test': 'true'})
 
-with open(in_file,'w') as file:
-	file.write(thatfile.content)
+with open(in_file, 'wt') as file:
+	file.write(str(thatfile.content))
 file.close()
 
 #-----------------------------------------------------------------------------------------
 
-print "Grooming the File"
-with open(in_file, 'rb')as groom:
+print("Grooming the File")
+with open(in_file, 'rt')as groom:
 	reader=csv.reader(groom, delimiter="|")
 	writer=csv.writer(dest, delimiter="|")
 
